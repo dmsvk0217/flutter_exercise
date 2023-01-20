d154073bb5e2cb6057cf8bac712d511efdd10afd
dmsvk0217 <dmsvk0217@gmail.com>
2023-01-20 18:09:33 +0900

    theme module 분리:

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,13 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application/screen/Duration_timer/timer_duration.dart';
+import 'package:flutter_application/screen/UandI/theme_uandi.dart';
 import 'package:flutter_application/screen/UandI/u_i_homescreen.dart';
 
 void main(List<String> args) {
   runApp(
     MaterialApp(
-      theme: ThemeData(
-        fontFamily: 'nanum',
-        textTheme: TextTheme(
-          headline1: TextStyle(
-            color: Colors.white,
-            fontSize: 50,
-          ),
-          headline2: TextStyle(
-            color: Colors.white,
-            fontSize: 30,
-          ),
-          bodyText1: TextStyle(
-            color: Colors.white,
-            fontSize: 30,
-          ),
-          bodyText2: TextStyle(
-            color: Colors.white,
-            fontSize: 50,
-          ),
-        ),
-      ),
+      theme: themeData,
       home: UandIHomescreen(),
       debugShowCheckedModeBanner: false,
     ),
---
+++ lib/screen/UandI/theme_uandi.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+var themeData = ThemeData(
+  fontFamily: 'nanum',
+  textTheme: TextTheme(
+    headline1: TextStyle(
+      color: Colors.white,
+      fontSize: 50,
+    ),
+    headline2: TextStyle(
+      color: Colors.white,
+      fontSize: 30,
+    ),
+    bodyText1: TextStyle(
+      color: Colors.white,
+      fontSize: 30,
+    ),
+    bodyText2: TextStyle(
+      color: Colors.white,
+      fontSize: 50,
+    ),
+  ),
+);
